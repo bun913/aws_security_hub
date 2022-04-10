6368461ef9f951720eb3abb99f5c5576608b6063
bun913 <imanau03@gmail.com>
2022-04-11 06:05:13 +0900

    feat: Slackに投稿されるまでの手順を追加

--- infra/global/modules/notification/main.tf
+++ infra/global/modules/notification/main.tf
@@ -53,7 +53,7 @@ resource "aws_cloudwatch_event_target" "sns-publish" {
 # マネジメントコンソールからChatBotを利用する際に指定する
 resource "aws_sns_topic" "chatbot" {
   name              = "${var.prefix}-topic"
-  kms_master_key_id = "alias/aws/sns"
+  kms_master_key_id = aws_kms_key.for_encrypt_sns_topic.id
 }
 
 resource "aws_sns_topic_policy" "chatbot" {
@@ -63,7 +63,7 @@ resource "aws_sns_topic_policy" "chatbot" {
 
 data "aws_iam_policy_document" "chatbot" {
   statement {
-    sid       = "Allow CloudwatchEvents"
+    sid       = "Allow CloudwatchEvents Publish"
     actions   = ["sns:Publish"]
     resources = [aws_sns_topic.chatbot.arn]
 
@@ -71,5 +71,60 @@ data "aws_iam_policy_document" "chatbot" {
       type        = "Service"
       identifiers = ["events.amazonaws.com"]
     }
+  }
+}
+
+resource "aws_kms_key" "for_encrypt_sns_topic" {
+  description         = "guarddutyからのeventを受けるsns topic暗号化用"
+  enable_key_rotation = true
+  policy              = data.aws_iam_policy_document.policy_for_encrypt_sns_topic.json
+}
+
+
+resource "aws_kms_alias" "for_encrypt_sns_topic_alias" {
+  name          = "alias/guardduty/for_encrypt_sns_topic"
+  target_key_id = aws_kms_key.for_encrypt_sns_topic.key_id
+}
+
+data "aws_iam_policy_document" "policy_for_encrypt_sns_topic" {
+  version = "2012-10-17"
+
+  # defaultでついてくるルートアカウントに対する権限設定
+  statement {
+    sid    = "Enable Root User Permissions"
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${var.account_id}:root"]
+    }
+
+    actions = [
+      "kms:*"
+    ]
+
+    resources = [
+      "*",
+    ]
+  }
+
+  # events.amazonaws.com に対する権限が暗号化対象のサービス操作に必要
+  statement {
+    sid    = "AWSEvents"
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["events.amazonaws.com"]
+    }
+
+    actions = [
+      "kms:GenerateDataKey",
+      "kms:Decrypt"
+    ]
+
+    resources = [
+      "*",
+    ]
   }
 }
--- infra/global/modules/notification/variables.tf
+++ infra/global/modules/notification/variables.tf
@@ -1,3 +1,6 @@
 variable "prefix" {
   type = string
 }
+variable "account_id" {
+  type = string
+}
